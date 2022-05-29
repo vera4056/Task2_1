c08ef8ff862c66756144b2b4cdd2461ac7237265
vera4056 <103599166+vera4056@users.noreply.github.com>
2022-05-29 17:59:16 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 100;
+        int bonus = 20;
+        int miles = cost / bonus;
+        String name = "Миль";
+
+
+        System.out.println(miles + name);
+    }
+}
